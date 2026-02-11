4757c95fa732b38df07a1f6fa663018332fdbc41
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-02-11 20:25:53 +0000

    xdfcgvbhk

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
+function oldEnough(age){
+    if (age > 17){
+        return "old enough";
+    }
+}
 
+function underAge(age){
+    if (age < 18){
+        return "under age";
+    }
+}
+
+function legalSpeed(speed){
+    if (speed <= 50){
+        return "legal speed";
+    }
+
+}
+
+function isGoldenYears(age){
+    if (age >= 65){
+        return "golden years";
+    }
+
+}
+//should create a function named legalSpeed that takes a speed and 
+//returns the string legal speed if the speed is under 50
+
+//should return the string legal speed if a speed of 50 is passed to 
+//legalSpeed
+
+//should create a function named isGoldenYears that takes an age and
+//returns the string golden years if the age is over 65
+
+//should return the string golden years if an age of 65 is passed to
+//isGoldenYears
+
+
+
+
+
+
+
+//should create a function named oldEnough that takes an age and 
+//returns the string old enough if the age is over 17
+
+//should not return the string old enough if an age of 17 is passed to 
+//oldEnough
+
+//should create a function named underAge that takes an age and returns
+//the string under age if the age is under 18
+
+//should not return the string under age if an age of 18 is passed to 
+//underAge
